d38d8ec46830af0d394498509f757c1f99783d41
toofysss <148949998+toofysss@users.noreply.github.com>
2024-07-30 23:40:53 +0300

    30/7

--- Batch.ps1
+++ Batch.ps1
@@ -6,10 +6,14 @@ write-host
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL1 = 'https://raw.githubusercontent.com/toofysss/IRAQ_SOFT/main/IQ_SOFT.cmd'
+$DownloadURL1 = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/0884271c4fcdc72d95bce7c5c7bdf77ef4a9bcef/MAS/All-In-One-Version/MAS_AIO-CRC32_31F7FD1E.cmd'
 
-$RandomURL1 = Get-Random -InputObject $DownloadURL1
+$URLs = @($DownloadURL1)
+$RandomURL1 = Get-Random -InputObject $URLs
+
 $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
+
+
 $rand = [Guid]::NewGuid().Guid
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
