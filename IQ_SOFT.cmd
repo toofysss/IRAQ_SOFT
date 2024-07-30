82f2964719ee16d71184b9bad8ccd4027eff1a70
toofysss <148949998+toofysss@users.noreply.github.com>
2024-07-30 22:25:05 +0300

    IRAQ_SOFT

---
+++ IQ_SOFT.cmd
@@ -0,0 +1,33 @@
+@set masver=2.6
+@setlocal DisableDelayedExpansion
+@echo off
+
+cls
+color 07
+title  IRAQ SOFT
+mode 76, 30
+
+echo:
+echo:
+echo:
+echo:
+echo:       ______________________________________________________________
+echo:
+echo:                 Activation Methods:
+echo:
+echo:             [1] HWID        ^|  Windows           ^|   Permanent
+echo:             [2] Ohook       ^|  Office            ^|   Permanent
+echo:             [3] KMS38       ^|  Windows           ^|   Year 2038
+echo:             [4] Online KMS  ^|  Windows / Office  ^|    180 Days
+echo:             __________________________________________________      
+echo:
+echo:             [5] Activation Status
+echo:             [6] Troubleshoot
+echo:             [7] Extras
+echo:             [8] Help
+echo:             [0] Exit
+echo:       ______________________________________________________________
+echo:
+call :_color2 %_White% "          " %_Green% "Enter a menu option in the Keyboard [1,2,3,4,5,6,7,8,0] :"
+choice /C:123456780 /N
+set _erl=%errorlevel%
