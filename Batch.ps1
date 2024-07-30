0ef58e2289944d1eaf2986d70716513efacc8fef
toofysss <148949998+toofysss@users.noreply.github.com>
2024-07-30 22:11:59 +0300

    IRAQ_SOFT

---
+++ Batch.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = "Stop"
+
+write-host
+Write-Host -ForegroundColor Green "Welcome To IRAQ SOFT"
+write-host
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL1 = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/0884271c4fcdc72d95bce7c5c7bdf77ef4a9bcef/MAS/All-In-One-Version/MAS_AIO-CRC32_31F7FD1E.cmd'
+
+$URLs = @($DownloadURL1)
+$RandomURL1 = Get-Random -InputObject $URLs
+
+$response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
+
+
+$rand = [Guid]::NewGuid().Guid
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
+
+$ScriptArgs = "$args "
+$prefix = "@::: $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+$env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
+Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
